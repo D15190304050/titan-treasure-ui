21dc2631ac7cdebbd36f8f5112a83f15e27d14e1
DinoStark <15190304050@163.com>
2025-10-28 20:28:51 +0800

    getProfileInfo

--- src/apis/profile.ts
+++ src/apis/profile.ts
@@ -1,7 +1,8 @@
 import { ServiceResponse } from '@/types';
 import axiosWithInterceptor from './axios';
+import { FullUserProfileInfo } from '@/types/profile';
 
-export async function getProfileInfo(): Promise<ServiceResponse<any>>
+export async function getProfileInfo(): Promise<ServiceResponse<FullUserProfileInfo>>
 {
     const response = await axiosWithInterceptor.get('/api/profile/info');
     return response.data;
